79d224f7b9a3f92bb404803e5195606784dbd6d5
nxfive <kontakt@nxfive.pl>
2026-01-02 23:30:37 +0100

    feat: Define numeric, categorical, and binary columns in features.py

---
+++ src/data/features.py
@@ -0,0 +1,24 @@
+CATEGORICAL = [
+    "gender",
+    "profession",
+    "sleep_duration",
+    "dietary_habits",
+]
+
+BINARY = [
+    "suicidal_thoughts",
+    "family_mental_illness",
+]
+
+NUMERIC = [
+    "age",
+    "academic_pressure",
+    "work_pressure",
+    "cgpa",
+    "study_satisfaction",
+    "job_satisfaction",
+    "degree",
+    "work_or_study_hours",
+    "financial_stress",
+]
+
