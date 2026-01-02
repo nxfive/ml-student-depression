9768b2a65bea2bad4dcc3b2ecb891fd4f2683149
nxfive <kontakt@nxfive.pl>
2026-01-02 23:26:50 +0100

    feat: Define schema constants for features, metadata, and target

---
+++ src/data/schema.py
@@ -0,0 +1,25 @@
+FEATURES = [
+    "gender",
+    "age",
+    "profession",
+    "academic_pressure",
+    "work_pressure",
+    "cgpa",
+    "study_satisfaction",
+    "job_satisfaction",
+    "sleep_duration",
+    "dietary_habits",
+    "degree",
+    "suicidal_thoughts",
+    "work_or_study_hours",
+    "financial_stress",
+    "family_mental_illness",
+]
+
+METADATA_FEATURES = [
+    "city",
+]
+
+TARGET = [
+    "depression",
+]
