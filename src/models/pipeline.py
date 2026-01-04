16b4e2f064e34a03b2a4cbfb72091595487c4ab1
nxfive <kontakt@nxfive.pl>
2026-01-05 00:28:30 +0100

    feat: Add function to create pipeline for Logistic Regression model

--- src/models/pipeline.py
+++ src/models/pipeline.py
@@ -1,4 +1,5 @@
 from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
@@ -25,3 +26,24 @@ def create_knn_pipeline() -> Pipeline:
     )
 
     return Pipeline([("preprocessor", preprocessor), ("model", KNeighborsClassifier())])
+
+
+def create_lr_pipeline() -> Pipeline:
+    """
+    Create a scikit-learn Pipeline for Logistic Regression classification.
+    """
+    preprocessor = ColumnTransformer(
+        [
+            ("num", StandardScaler(), NUMERIC),
+            ("bin", "passthrough", BINARY),
+            (
+                "cat",
+                OneHotEncoder(
+                    drop="first", sparse_output=False, handle_unknown="ignore"
+                ),
+                CATEGORICAL,
+            ),
+        ]
+    )
+
+    return Pipeline([("preprocessor", preprocessor), ("model", LogisticRegression())])
